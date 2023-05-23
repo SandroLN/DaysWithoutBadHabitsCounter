b6e41799b60b38af5d813ad0ad405dc0aebc9db7
Nikolai Lisenko <kollysenko@gmail.com>
2023-05-23 19:08:30 +0500

    repository after test

--- app/src/main/java/com/sandroln/dayswithoutbadhabits/MainRepository.kt
+++ app/src/main/java/com/sandroln/dayswithoutbadhabits/MainRepository.kt
@@ -1,8 +1,60 @@
 package com.sandroln.dayswithoutbadhabits
 
+import android.content.SharedPreferences
+
 interface MainRepository {
 
     fun reset()
 
     fun days(): Int
+
+    class Base(
+        private val cacheDataSource: CacheDataSource,
+        private val now: Now
+    ) : MainRepository {
+
+        override fun reset() = cacheDataSource.save(now.time())
+
+        override fun days(): Int {
+            val saved = cacheDataSource.time(-1)
+            return if (saved == -1L) {
+                reset()
+                0
+            } else {
+                val dif = now.time() - saved
+                (dif / DAY_MILLIS).toInt()
+            }
+        }
+
+
+        private companion object {
+            private const val DAY_MILLIS = 24 * 3600 * 1000
+        }
+    }
+}
+
+interface Now {
+    fun time(): Long
+
+    class Base() : Now {
+        override fun time(): Long = System.currentTimeMillis()
+    }
+}
+
+interface CacheDataSource {
+    fun save(time: Long)
+    fun time(default: Long): Long
+
+    class Base(private val sharedPreferences: SharedPreferences) : CacheDataSource {
+
+        override fun save(time: Long) = sharedPreferences.edit().putLong(KEY, time).apply()
+
+
+        override fun time(default: Long): Long = sharedPreferences.getLong(KEY, default)
+
+        companion object {
+            private const val KEY = "savedTimeKey"
+        }
+
+    }
 }
